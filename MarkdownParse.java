c1b76dc4d93787c481148231c1290782313b1189
jeyiu <jeyiu@ucsd.edu>
2022-04-22 16:08:15 -0700

    Forgotten Update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,8 +24,7 @@ public class MarkdownParse {
                 if(openParen == closeBracket+1){
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
                 }
-                currentIndex = closeParen + 1;
-                
+                currentIndex = closeParen + 1;   
             }
             
         }
